2c7b1bbd1c08a3f6c7b2369f0049ce10c8e9e289
AndroGenius-codes <70571088+AndroGenius-codes@users.noreply.github.com>
2022-09-09 23:18:56 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
  * main - Entry point
- *
- * Description -  program will assign a random number to the variable n
  *
  * Return: Alwayys 0 (Success)
  */
