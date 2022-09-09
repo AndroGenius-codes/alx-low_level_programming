30862dc210762de05190878dc182f4d2a55f357d
AndroGenius-codes <70571088+AndroGenius-codes@users.noreply.github.com>
2022-09-10 00:27:15 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
 /**
- * main - determine if number is positive or negative
- *
- * Description: program will assign a random number to the variable n each time
- * it is executed.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	return(0);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
 }
