2925a5acef4257fdfa0015c549522ff9ca6e35a3
AndroGenius-codes <70571088+AndroGenius-codes@users.noreply.github.com>
2022-09-14 00:22:38 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * print_sign - Checks sign of character
- * @n: Receives a character argument
- *
- * Return: int
- */
+ * print_sign - print sign of a number
+ * @n: int number
+ * Return: 1 if positive, 0 if 0, -1 if negative
+*/
+
 int print_sign(int n)
 {
-	int res = 0;
-
 	if (n > 0)
 	{
 		_putchar('+');
-		res = 1;
+		return (1);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
-		res = 0;
+		return (0);
 	}
-	else
+	if (n < 0)
 	{
 		_putchar('-');
-		res = -1;
+		return (-1);
 	}
-	return (res);
+	return (2);
 }
