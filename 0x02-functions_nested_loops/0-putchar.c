2414ad6304e717bbab3775fd0457cd94b386ca11
AndroGenius-codes <70571088+AndroGenius-codes@users.noreply.github.com>
2022-09-13 18:57:51 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int str[] = {45, 112, 117, 116, 99, 104, 97, 114};
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i, size;
 
 	size = sizeof(str) / sizeof(int);
 	for (i = 0; i < size; i++)
-	{
+        {
 		_putchar(str[i]);
 	}
 	_putchar('\n');
