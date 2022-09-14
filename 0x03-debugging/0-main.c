532138b74481c4e1f93673d1aee9523c8351d0fe
Living <livingenaanasebh@gmail.com>
2022-09-14 23:55:26 +0100

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
-/**                                               * main - tests function that prints if integer 0  *is positive or negative
-*
-* Return: 0
-*/
+/**
+ * main - tests function that prints if integer
+ * is positive or negative
+ *
+ * Return: 0
+ */
 
 int main(void)
 {
-        int i = 0;
+        int i;
 
+	i = 0;
         positive_or_negative(i);
 
         return (0);
