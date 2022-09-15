db059118987dac58468609501ae4c6132930fcb4
Living <livingenaanasebh@gmail.com>
2022-09-15 01:48:04 +0100

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,24 +8,24 @@
 
 int main(void)
 {
-		long int total_sum, sum, first, second;
+	long int total_sum, sum, first, second;
 
-			total_sum = 0;
-				sum = 0;
-					first = 0;
-						second = 1;
+	total_sum = 0;
+	sum = 0;
+	first = 0;
+	second = 1;
+	while (sum < 4000000)
+	{
+		sum = first + second;
+		if (sum % 2 == 0)
+		{
+			total_sum += sum;
+		}
 
-							while (sum < 4000000)
-							{
-								sum = first + second;
-								if (sum % 2 == 0)
-								{
-									total_sum += sum;
-								}
-								first = second;
-								second = sum;
-							}
+		first = second;
+		second = sum;
+	}
+	printf("%li\n", total_sum);
 
-							printf("%li\n", total_sum);
-							return (0);
+	return (0);
 }
